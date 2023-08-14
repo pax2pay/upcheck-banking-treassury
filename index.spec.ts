a40ce62a5822813ed34b6841630286aa1c3f3bdf
Nils Hansander <nilshansander@hotmail.com>
2023-08-14 16:28:46 +0200

    test e-money vs fiat balances (#5)
    * test e-money vs fiat balances
    
    * Better descriptions
    
    * logs and improved test output

--- index.spec.ts
+++ index.spec.ts
@@ -1,8 +1,33 @@
+import { gracely } from "gracely"
 import "jest"
-import * as library from "./index"
+import { pax2pay } from "@pax2pay/model-banking"
+import { userwidgets } from "@userwidgets/model"
+import * as dotenv from "dotenv"
 
-describe("library", () => {
-	it("a", () => {
-		expect(library.a).toEqual(42)
+dotenv.config()
+
+const client = process.env.url ? pax2pay.Client.create(process.env.url, "") : undefined
+client && (client.realm = "testUK")
+let token: string | gracely.Error
+
+describe("Compare fiat and e-money", () => {
+	beforeAll(async () => {
+		const key = await client?.userwidgets.me.login({
+			user: process.env.email ?? "",
+			password: process.env.password ?? "",
+		})
+		token = userwidgets.User.Key.is(key) ? key.token : gracely.client.unauthorized()
+		typeof token == "string" && client && (client.key = token)
+	})
+	it("get token", async () => {
+		expect(typeof token == "string").toBeTruthy()
+	})
+	it("e-money less or equal to safeguarded funds", async () => {
+		const fiat = await client?.treasury.fetch()
+		expect(gracely.Error.is(fiat)).toBe(false)
+		console.log(fiat)
+		Object.entries(fiat as pax2pay.Treasury).forEach(balance =>
+			expect(balance[1].fiat.safe).toBeGreaterThanOrEqual(balance[1].eMoney.actual ?? 0)
+		)
 	})
 })
